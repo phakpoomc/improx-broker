81477c7ff90ec28c89da8c2bee5e802b1898fc3c
ninerxsolution <prisonercastgamingch@gmail.com>
2024-03-19 12:42:12 +0700

    added montserrat google font

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import Home from './Home.vue'
 import router from './router'
-import "./assets/js/bootstrap.js"
+import './assets/js/bootstrap.js'
+import './assets/css/style.scss'
 
 createApp(Home).use(router).mount('#app')
